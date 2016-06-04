b073f95c3ec95168650161b26be4ca219886a92f
David Hayes <d.hayes@accenture.com>
2016-06-04 20:57:44 +0100

    Correct an embarassing syntax error

--- tests/init.pp
+++ tests/init.pp
@@ -9,14 +9,13 @@
 # Learn more about module testing here:
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
-class { 'netrc':
-   user => 'myuser',
-   path => '/home/myuser/.netrc',
+class { '::netrc':
+  user => 'myuser',
+  path => '/home/myuser/.netrc',
 }
 
 netrc::machine { 'ftp.foo.com':
-   netrc_path => '/home/myuser/.netrc',
-   login      => 'remote_user',
-   password   => 'hunter5',
-}
+  netrc_path => '/home/myuser/.netrc',
+  login      => 'remote_user',
+  password   => 'hunter5',
 }
